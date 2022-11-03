afe6fe3f58319d56ce347bf9a54f46114e11e0f0
Bikram-Gyawali <bikramgyawali57@gmail.com>
2022-11-03 22:54:23 +0545

    video downloader for youtube

--- youtubevideo_downloader/index.js
+++ youtubevideo_downloader/index.js
@@ -5,18 +5,20 @@ const ytdl = require("ytdl-core");
 app.set("view engine", "ejs"); // OUR ROUTES WILL GO HERE
 
 app.get("/", (req, res) => {
-	return res.render("index");
+  return res.render("index");
 });
 
-
-//route to download 
-app.get('\download' , (req, res) => {
-    const v_id = req.query.url.split('v=')[1];
-    return res.render('download', {
-        url: "https://www.youtube.com/embed/" + v_id,
-    });
+//route to download
+app.get("/download", async (req, res) => {
+  const v_id = req.query.url.split("v=")[1];
+  const info = await ytdl.getInfo(req.query.url);
+  return res.render("download", {
+    url: "https://www.youtube.com/embed/" + v_id,
+    info: info.formats.sort((a, b) => {
+      return a.mimeType < b.mimeType;
+    }),
+  });
 });
-
 
 app.listen(port, () => {
   console.log("Server is running on http://localhost:" + port);
