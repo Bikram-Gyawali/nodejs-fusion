8a9a62806c9324a4723e6d39f57e804ef9a9d606
Bikram-Gyawali <bikramgyawali57@gmail.com>
2022-11-09 18:01:01 +0545

    db connected

--- sorting-and-searching/server.js
+++ sorting-and-searching/server.js
@@ -1,8 +1,10 @@
 require("dotenv").config();
 const express = require("express");
 const cors = require("cors");
+const dbConnect = require("./dbConnect");
 const app = express();
 
+dbConnect();
 app.use(express.json());
 app.use(cors());
 
