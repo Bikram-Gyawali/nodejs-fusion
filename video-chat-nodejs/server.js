b897424522e66e0a301049fce8365452db05624f
Bikram-Gyawali <bikramgyawali57@gmail.com>
2022-11-16 22:57:24 +0545

    some ide errors maybe showing many bugs

--- video-chat-nodejs/server.js
+++ video-chat-nodejs/server.js
@@ -1,7 +1,11 @@
 const express = require(“express”);
 const app = express();
 const server = require(“http”).Server(app);
+
+
+app.set(‘view engine’, ‘ejs’)
+
 app.get(“/”, (req, res) => {
     res.status(200).send(“Hello World”);
 });
-server.listen(3030);
+server.listen(3000);
