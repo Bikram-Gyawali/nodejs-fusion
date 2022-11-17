dffeaa55a50e342fdcf4283070a4323dbbc180f1
Bikram-Gyawali <bikramgyawali57@gmail.com>
2022-11-17 23:29:08 +0545

    server file changes for render room.ejs

--- video-chat-nodejs/server.js
+++ video-chat-nodejs/server.js
@@ -5,7 +5,7 @@ const server = require(“http”).Server(app);
 
 app.set(‘view engine’, ‘ejs’)
 
-app.get(“/”, (req, res) => {
-    res.status(200).send(“Hello World”);
-});
+app.get("/", function (req, res) {
+    res.render(‘room’);
+   })
 server.listen(3000);
