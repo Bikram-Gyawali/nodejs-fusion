0db4d81c5dfb40020d31c251063faabd48d6e10d
Bikram-Gyawali <bikramgyawali57@gmail.com>
2022-11-17 23:31:52 +0545

    server file changes for render and static public folder linking

--- video-chat-nodejs/server.js
+++ video-chat-nodejs/server.js
@@ -1,11 +1,11 @@
-const express = require(“express”);
+const express = require("express");
 const app = express();
-const server = require(“http”).Server(app);
+const server = require("http").Server(app);
 
+app.set("view engine", "ejs");
 
-app.set(‘view engine’, ‘ejs’)
-
+app.use(express.static("public"));
 app.get("/", function (req, res) {
-    res.render(‘room’);
-   })
+  res.render("room");
+});
 server.listen(3000);
