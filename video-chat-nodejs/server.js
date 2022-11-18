9ba10c7ef9396309a70c930b78e68212a7881f1c
Bikram-Gyawali <bikramgyawali57@gmail.com>
2022-11-18 23:32:38 +0545

    uncommited changes in folder and file

--- video-chat-nodejs/server.js
+++ video-chat-nodejs/server.js
@@ -2,9 +2,9 @@ const express = require("express");
 const app = express();
 const server = require("http").Server(app);
 
+app.use(express.static("public"));
 app.set("view engine", "ejs");
 
-app.use(express.static("public"));
 app.get("/", function (req, res) {
   res.render("room");
 });
