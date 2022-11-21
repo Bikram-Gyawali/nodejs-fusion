425568d687d00d631c21bcfc2769f8f6fbcead6e
Bikram-Gyawali <bikramgyawali57@gmail.com>
2022-11-21 23:40:06 +0545

    io connection socket

--- video-chat-nodejs/server.js
+++ video-chat-nodejs/server.js
@@ -5,6 +5,12 @@ const { v4: uuidv4 } = require("uuid");
 app.use(express.static("public"));
 app.set("view engine", "ejs");
 
+const io = require("socket.io")(server);
+const { ExpressPeerServer } = require("peer");
+const { ExpressPeerServer } = require("peer");
+const peerServer = ExpressPeerServer(server, { debug: true });
+app.use("/peerjs", peerServer);
+
 app.get("/", (req, res) => {
   res.redirect("${uuidv4()}");
 });
@@ -16,4 +22,9 @@ app.get("/", function (req, res) {
 app.get("/:room", (req, res) => {
   res.render("room", { roomId: req.param.room });
 });
+
+
+io.on("connection", (socket) => {socket.on("join-room", (roomId, userId) => {socket.join(roomId);
+    socket.to(roomId).broadcast.emit("user-connected", userId);
+
 server.listen(3000);
