039283661dbb27b040a340a043a5c5291980e434
Bikram-Gyawali <bikramgyawali57@gmail.com>
2022-11-22 23:33:56 +0545

    uuid import and redirect to the particular uuid

--- video-chat-nodejs/server.js
+++ video-chat-nodejs/server.js
@@ -1,9 +1,13 @@
 const express = require("express");
 const app = express();
 const server = require("http").Server(app);
-
+const { v4: uuidv4 } = require("uuid");
 app.use(express.static("public"));
 app.set("view engine", "ejs");
+
+app.get("/", (req, res) => {
+  res.redirect("${uuidv4()}");
+});
 
 app.get("/", function (req, res) {
   res.render("room");
