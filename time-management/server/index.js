ca4a8bf4ce1349647d54df0bdb2a8cfca391a4d2
Bikram-Gyawali <bikramgyawali57@gmail.com>
2022-12-04 18:09:41 +0545

    routes and services

--- time-management/server/index.js
+++ time-management/server/index.js
@@ -3,22 +3,12 @@ const cors = require("cors");
 const bodyParser = require("body-parser");
 const { Sequelize } = require("sequelize");
 const path = require("path");
+
 const db = new Sequelize("task_schedule", "username", "password", {
   host: "localhost",
   dialect: "mysql",
   operatorAliases: false,
   pool: { max: 5, min: 0, acquire: 30000, idle: 10000 },
-});
-const PORT = 3001;
-const app = express();
-app.use(cors());
-app.use(bodyParser.json());
-app.use(express.static(path.join(__dirname + "/../client")));
-app.get("/api/test", (req, res) => {
-  res.send("Hello world!");
-});
-app.listen(PORT, () => {
-  console.log(`App is live on port ${PORT}`);
 });
 
 const Task = db.define(
@@ -38,6 +28,7 @@ const Task = db.define(
     timestamps: false,
   }
 );
+
 const TaskNote = db.define(
   "task_note",
   {
@@ -50,35 +41,68 @@ const TaskNote = db.define(
     time: Sequelize.DATE,
     content: Sequelize.TEXT,
   },
-
-   
   {
     freezeTableName: true,
     timestamps: false,
   }
 );
 
-
 const TaskFunctions = {
-    async getAll() {
-        return Task.findAll();
-    },    async get(id) {
-        return Task.findByPk(id);
-    },    async create(data) {
-        const task = Task.create({
-            name: data.name,
-            cron_string: data.cron_string
-        });
-        return task;
-    },    async update(id, data) {
-        const task = await Task.findByPk(id);
-        task.name = data.name || task.name;
-        task.cron_string = data.cron_string || task.cron_string;
-        task.completed = [true, false].includes(data.completed) ? data.completed : task.completed;
-        await task.save();
-        return task;
-    },    async del(id) {
-        const task = await Task.destroy({where: {id}});
-        return id;
-    }
-}
+  async getAll() {
+    return Task.findAll();
+  },
+
+  async get(id) {
+    return Task.findByPk(id);
+  },
+
+  async create(data) {
+    const task = Task.create({
+      name: data.name,
+      cron_string: data.cron_string,
+    });
+    return task;
+  },
+
+
+};
+
+const PORT = 3001;
+const app = express();
+
+app.use(cors());
+app.use(bodyParser.json());
+app.use(express.static(path.join(__dirname + "/../client")));
+
+app.get("/api/test", (req, res) => {
+  res.send("Hello world!");
+});
+
+app.get("/api/task/all", async (req, res) => {
+  const result = await TaskFunctions.getAll();
+  res.send({ result });
+});
+
+app.get("/api/task/:id", async (req, res) => {
+  const result = await TaskFunctions.get(req.params.id);
+  res.send({ result });
+});
+
+app.post("/api/task", async (req, res) => {
+  const result = await TaskFunctions.create(req.body);
+  res.send({ result });
+});
+
+app.put("/api/task/:id", async (req, res) => {
+  const result = await TaskFunctions.update(req.params.id, req.body);
+  res.send({ result });
+});
+
+app.delete("/api/task/:id", async (req, res) => {
+  const result = await TaskFunctions.del(+req.params.id);
+  res.send({ result });
+});
+
+app.listen(PORT, () => {
+  console.log(`App is live on port ${PORT}`);
+});
