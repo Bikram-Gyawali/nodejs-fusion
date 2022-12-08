2ad686dc02f2f006f9a5ce2ab81ce8a7fb76fd65
Bikram-Gyawali <bikramgyawali57@gmail.com>
2022-12-09 00:03:46 +0545

    task table

--- time-management/client/task.js
+++ time-management/client/task.js
@@ -1,22 +1,61 @@
-const url = 'http://localhost:3001/api/task';
+import { useState, useEffect } from "react";
+const url = "http://localhost:3001/api/task";
 const headers = {
-    'Content-Type': 'application/json'
-};const API = {
-    async getAll() {
-        const res = await fetch(`${url}/all`, {method: 'GET', headers});
-        const {result} = await res.json();
-        return result;
-    },    async create(body) {
-        const res = await fetch(url, {method: 'POST', headers, body: JSON.stringify(body)});
-        const {result} = await res.json();
-        return result;
-    },    async update(id, body) {
-        const res = await fetch(`${url}/${id}`, {method: 'PUT', headers, body: JSON.stringify(body)});
-        const {result} = await res.json();
-        return result;
-    },    async del(id) {
-        const res = await fetch(`${url}/${id}`, {method: 'DELETE', headers});
-        const {result} = await res.json();
-        return result;
+  "Content-Type": "application/json",
+};
+const API = {
+  async getAll() {
+    const res = await fetch(`${url}/all`, { method: "GET", headers });
+    const { result } = await res.json();
+    return result;
+  },
+  async create(body) {
+    const res = await fetch(url, {
+      method: "POST",
+      headers,
+      body: JSON.stringify(body),
+    });
+    const { result } = await res.json();
+    return result;
+  },
+  async update(id, body) {
+    const res = await fetch(`${url}/${id}`, {
+      method: "PUT",
+      headers,
+      body: JSON.stringify(body),
+    });
+    const { result } = await res.json();
+    return result;
+  },
+  async del(id) {
+    const res = await fetch(`${url}/${id}`, { method: "DELETE", headers });
+    const { result } = await res.json();
+    return result;
+  },
+};
+
+const TaskTable = (props) => {
+  const [tasks, setTasks] = useState([]);
+  useEffect(() => {
+    async function getTasks() {
+      const data = await API.getAll();
+      setTasks(data);
     }
-}
+    getTasks();
+  }, []);
+  return (
+    <table border="1">
+      <thead>
+        <tr>
+          <th>Completed</th>
+          <th>Name</th>
+          <th>Cron String</th>
+          <th>Edit</th>
+          <th>Delete</th>
+        </tr>
+      </thead>
+      <tbody></tbody>
+    </table>
+  );
+};
+export default TaskTable;
