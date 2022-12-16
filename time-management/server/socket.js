3a681e00d7f93aed67d62473dce1f88b1acb5398
Bikram-Gyawali <bikramgyawali57@gmail.com>
2022-12-16 23:49:36 +0545

    sockt server set

--- time-management/server/socket.js
+++ time-management/server/socket.js
@@ -1,16 +1,19 @@
-const {WebSocketServer, OPEN} = require('ws');class Socket {
-    constructor() {
-        this.wss = new WebSocketServer({port: 8080});
-        this.wss.on('connection', ws => console.log('Connected!'));
-    }    async sendTask(task) {
-        this.wss.clients.forEach(client => {
-            if (client.readyState === OPEN) {
-                const data = {
-                    type: 'task_start',
-                    task
-                };
-                client.send(JSON.stringify(data));
-            }
-        });
-    }
-}module.exports = Socket;
+const { WebSocketServer, OPEN } = require("ws");
+class Socket {
+  constructor() {
+    this.wss = new WebSocketServer({ port: 8080 });
+    this.wss.on("connection", (ws) => console.log("Connected!"));
+  }
+  async sendTask(task) {
+    this.wss.clients.forEach((client) => {
+      if (client.readyState === OPEN) {
+        const data = {
+          type: "task_start",
+          task,
+        };
+        client.send(JSON.stringify(data));
+      }
+    });
+  }
+}
+module.exports = Socket;
