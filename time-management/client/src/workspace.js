edc27160eb70729f5be2cae239ebcbe4e687e95f
Bikram-Gyawali <bikramgyawali57@gmail.com>
2022-12-19 23:54:36 +0545

    fixed errors

--- time-management/client/src/workspace.js
+++ time-management/client/src/workspace.js
@@ -1,4 +1,4 @@
-import {useState, useEffect} from 'react';
+import { useState, useEffect } from "react";
 const url = "http://localhost:3001/api/task";
 const headers = {
   "Content-Type": "application/json",
@@ -15,41 +15,52 @@ const API = {
   },
 };
 
-
-const Workspace = props => {
-    const [task, setTask] = useState({});    useEffect(() => {
-        const socket = new WebSocket('ws://localhost:8080');
-        socket.onmessage = e => {
-            try {
-                const data = JSON.parse(e.data);
-                setTask(data.task);
-            } catch (e) {
-                console.log(e);
-            }
-        }
-    }, []);    const handleClick = async e => {
-        const {id, completed} = task;
-        const result = await API.update(id, {completed: !completed});
-        setTask({...task, completed: !completed});
-    };    if (task.id) {
-        return <>
-            <table border='1'>
-                <thead>
-                    <tr>
-                        <th>Completed</th>
-                        <th>Name</th>
-                    </tr>
-                </thead>
-                <tbody>
-                    <tr>
-                        <td><input type='checkbox' name='completed' checked={task.completed} onChange={handleClick} /></td>
-                        <td>{task.name}</td>
-                    </tr>
-                </tbody>
-            </table>
-        </>
-    } else {
-        return <b>Waiting for a task to start</b>
-    }
-}export default Workspace;
-
+const Workspace = (props) => {
+  const [task, setTask] = useState({});
+  useEffect(() => {
+    const socket = new WebSocket("ws://localhost:8080");
+    socket.onmessage = (e) => {
+      try {
+        const data = JSON.parse(e.data);
+        setTask(data.task);
+      } catch (e) {
+        console.log(e);
+      }
+    };
+  }, []);
+  const handleClick = async (e) => {
+    const { id, completed } = task;
+    const result = await API.update(id, { completed: !completed });
+    setTask({ ...task, completed: !completed });
+  };
+  if (task.id) {
+    return (
+      <>
+        <table border="1">
+          <thead>
+            <tr>
+              <th>Completed</th>
+              <th>Name</th>
+            </tr>
+          </thead>
+          <tbody>
+            <tr>
+              <td>
+                <input
+                  type="checkbox"
+                  name="completed"
+                  checked={task.completed}
+                  onChange={handleClick}
+                />
+              </td>
+              <td>{task.name}</td>
+            </tr>
+          </tbody>
+        </table>
+      </>
+    );
+  } else {
+    return <b>Waiting for a task to start</b>;
+  }
+};
+export default Workspace;
