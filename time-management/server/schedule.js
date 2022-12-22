3a383dce2d1931322f482fd051765178a0dee4fe
Bikram-Gyawali <bikramgyawali57@gmail.com>
2022-12-22 18:55:48 +0545

    task schedule class changed updates

--- time-management/server/schedule.js
+++ time-management/server/schedule.js
@@ -1,32 +1,41 @@
-const schedule = require("node-schedule");
+const schedule = require('node-schedule');
+
 class TaskSchedule {
-  constructor(taskFns) {
-    this.taskFns = taskFns;
-    process.on("SIGINT", async () => {
-      await schedule.gracefulShutdown();
-      process.exit(0);
-    });
-  }
-  addTask(task) {
-    const { id, cron_string } = task;
-    const job = schedule.scheduleJob(`job:${id}`, cron_string, () => {
-      // Put socket message here
-    });
-    return job;
-  }
-  async populateTasks() {
-    const tasks = await this.taskFns.getIncomplete();
-    return tasks.map((t) => this.addTask(t));
-  }
-  clearTasks() {
-    Object.keys(schedule.scheduledJobs).map((k) => {
-      const job = schedule.scheduledJobs[k];
-      job.cancel();
-    });
-  }
-  async rebuildSchedule() {
-    this.clearTasks();
-    await this.populateTasks();
-  }
+    constructor(taskFns, socket) {
+        this.taskFns = taskFns;
+        this.socket = socket;
+
+        process.on('SIGINT', async () => {
+            await schedule.gracefulShutdown();
+            process.exit(0);
+        });
+    }
+
+    addTask(task) {
+        const {id, cron_string} = task;
+        const job = schedule.scheduleJob(`job:${id}`, cron_string, () => {
+            this.socket.sendTask(task);
+        });
+        return job;
+    }
+
+    async populateTasks() {
+        const tasks = await this.taskFns.getIncomplete();
+        return tasks.map(t => this.addTask(t));
+    }
+
+    clearTasks() {
+        Object.keys(schedule.scheduledJobs).map(k => {
+            const job = schedule.scheduledJobs[k];
+            job.cancel();
+        });
+    }
+
+    async rebuildSchedule() {
+        this.clearTasks();
+        await this.populateTasks();
+    }
 }
+
 module.exports = TaskSchedule;
+view raw
