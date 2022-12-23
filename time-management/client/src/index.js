92c31b1ad76c6fc744b452366c7eea8ae728d056
Bikram-Gyawali <bikramgyawali57@gmail.com>
2022-12-23 19:12:30 +0545

    time managemenet frontend simple sturcture done

--- time-management/client/src/index.js
+++ time-management/client/src/index.js
@@ -3,15 +3,19 @@ import ReactDOM from "react-dom/client";
 import App from "./App";
 import reportWebVitals from "./reportWebVitals";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
-import Workspace from './workspace';
+import Workspace from "./workspace";
+import Home from "./home";
+import Nav from "./nav";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
     <BrowserRouter>
+      <Nav />
       <Routes>
         <Route path="/" element={<App />}></Route>
-        <Route path='workspace' element={<Workspace />} />
+        <Route path="" element={<Home />} />
+        <Route path="workspace" element={<Workspace />} />
       </Routes>
     </BrowserRouter>
   </React.StrictMode>
--- time-management/client/src/nav.js
+++ time-management/client/src/nav.js
@@ -1,8 +1,18 @@
-import { Link } from 'react-router-dom';
-import './App.css';const Nav = props => {
-    return <ul id='nav'>
-        <li><Link to='/'>Home</Link></li>
-        <li><Link to='/workspace'>Workspace</Link></li>
-        <li><Link to='/tasks'>Tasks</Link></li>
+import { Link } from "react-router-dom";
+import "./App.css";
+const Nav = (props) => {
+  return (
+    <ul id="nav">
+      <li>
+        <Link to="/">Home</Link>
+      </li>
+      <li>
+        <Link to="/workspace">Workspace</Link>
+      </li>
+      <li>
+        <Link to="/tasks">Tasks</Link>
+      </li>
     </ul>
-}export default Nav;
+  );
+};
+export default Nav;
