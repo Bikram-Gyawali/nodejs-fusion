b9aa4064fb1cb6f043596434a50a14554165a770
Bikram-Gyawali <bikramgyawali57@gmail.com>
2022-12-23 19:13:08 +0545

    task schedule class new function for getting task for range period

--- time-management/server/schedule.js
+++ time-management/server/schedule.js
@@ -1,40 +1,53 @@
-const schedule = require('node-schedule');
+const schedule = require("node-schedule");
 
 class TaskSchedule {
-    constructor(taskFns, socket) {
-        this.taskFns = taskFns;
-        this.socket = socket;
+  constructor(taskFns, socket) {
+    this.taskFns = taskFns;
+    this.socket = socket;
 
-        process.on('SIGINT', async () => {
-            await schedule.gracefulShutdown();
-            process.exit(0);
-        });
-    }
+    process.on("SIGINT", async () => {
+      await schedule.gracefulShutdown();
+      process.exit(0);
+    });
+  }
 
-    addTask(task) {
-        const {id, cron_string} = task;
-        const job = schedule.scheduleJob(`job:${id}`, cron_string, () => {
-            this.socket.sendTask(task);
-        });
-        return job;
-    }
+  addTask(task) {
+    const { id, cron_string } = task;
+    const job = schedule.scheduleJob(`job:${id}`, cron_string, () => {
+      this.socket.sendTask(task);
+    });
+    return job;
+  }
 
-    async populateTasks() {
-        const tasks = await this.taskFns.getIncomplete();
-        return tasks.map(t => this.addTask(t));
-    }
+  async populateTasks() {
+    const tasks = await this.taskFns.getIncomplete();
+    return tasks.map((t) => this.addTask(t));
+  }
 
-    clearTasks() {
-        Object.keys(schedule.scheduledJobs).map(k => {
-            const job = schedule.scheduledJobs[k];
-            job.cancel();
-        });
-    }
+  clearTasks() {
+    Object.keys(schedule.scheduledJobs).map((k) => {
+      const job = schedule.scheduledJobs[k];
+      job.cancel();
+    });
+  }
 
-    async rebuildSchedule() {
-        this.clearTasks();
-        await this.populateTasks();
-    }
+  async rebuildSchedule() {
+    this.clearTasks();
+    await this.populateTasks();
+  }
+
+  async getTasksForRange(start, end) {
+    const todayTasks = [];
+    Object.keys(schedule.scheduledJobs).map((k) => {
+      const job = schedule.scheduledJobs[k];
+      const next = job.nextInvocation().toDate().getTime();
+      if (next >= start && next <= end) {
+        const id = +job.name.split(":")[1];
+        todayTasks.push(id);
+      }
+    });
+    return todayTasks;
+  }
 }
 
 module.exports = TaskSchedule;
