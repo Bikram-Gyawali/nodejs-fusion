cbbea97efdd2c913363f4ccd54da844d15229e4d
Bikram-Gyawali <bikramgyawali57@gmail.com>
2022-12-31 23:54:54 +0545

    its working till now

--- remoteWork/index.js
+++ remoteWork/index.js
@@ -1,14 +1,25 @@
 require("dotenv").config();
-const express = require("express");
 const dbConnect = require("./db/dbConnect");
+const express = require("express");
 const app = express();
-const getBalance = require("./utils/getBalance");
-const uploadAddressandBalance = require("./routes/wallet.routes");
-dbConnect();
+const {
+  uploadWalletsandBalance,
+  getWalletsAndBalance,
+} = require("./routes/wallet.routes");
+const scheduledTask = require("./utils/scheduledFetch");
 
+//connect to database
+try {
+  dbConnect();
+} catch (error) {
+  throw new Error("Connection failed : ", { cause: error });
+}
 
 app.use(express.json());
-app.use("/api", uploadAddressandBalance);
+app.use("/api", uploadWalletsandBalance); // to upload wallets and balance uncomment this
+app.use("/api", getWalletsAndBalance);
+
+scheduledTask();
 
 const port = 3000;
 
