32adecc519b1d5596e80b43e66d00785771b93af
Bikram-Gyawali <bikramgyawali57@gmail.com>
2023-01-01 06:59:35 +0545

    strurcture changed

--- remoteWork/db/dbConnect.js
+++ remoteWork/db/dbConnect.js
@@ -1,18 +1,6 @@
 const mongoose = require("mongoose");
-const dbConnect = () => {
-  dbClient = mongoose.connect(process.env.DB, { useNewUrlParser: true });
-
-  mongoose.connection.on("connected", () => {
-    console.log("Connected to db ");
-  });
-
-  mongoose.connection.on("error", (err) => {
-    console.log("Error occured during connectiton", err);
-  });
-
-  mongoose.connection.on("disconnected", () => {
-    console.log("Conection terminated");
-  });
+const dbConnect = async () => {
+  await mongoose.connect(process.env.DB, { useNewUrlParser: true });
 };
 
 module.exports = dbConnect;
