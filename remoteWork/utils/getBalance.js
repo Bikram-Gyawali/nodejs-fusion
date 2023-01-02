baeb60607d126b12dc109df4f9d03e4672a33e58
Bikram-Gyawali <bikramgyawali57@gmail.com>
2023-01-02 07:06:35 +0545

    some changes in code refactored

--- remoteWork/utils/getBalance.js
+++ remoteWork/utils/getBalance.js
@@ -1,17 +1,21 @@
 const walletAddress = require("./addressList");
 const axios = require("axios");
+
 const getBalance = async () => {
   //   for (let i = 0; i < 20; i++) {
+  walletAddress.length = 5;
   try {
-    let response;
-    await axios
+    let response = await axios
       .get(
         `https://api.bscscan.com/api?module=account&action=balancemulti&address=${walletAddress}&tag=latest&apikey=${process.env.API_KEY}`
       )
-      .then((res) => (response = res.data.result), console.log("Response"));
+      .then(
+        (res) => res.data.result
+        // console.log("Response")
+      );
     return response;
   } catch (error) {
-    console.log("error occured", error);
+    // console.log("error occured");
     return error;
   }
 };
