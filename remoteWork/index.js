8bf0beba686435cce29251788e8fb80aa02e9060
Bikram-Gyawali <bikramgyawali57@gmail.com>
2023-01-03 11:57:00 +0545

    project completed

--- remoteWork/index.js
+++ remoteWork/index.js
@@ -7,20 +7,44 @@ const {
   getWalletsAndBalance,
 } = require("./routes/wallet.routes");
 const scheduledTask = require("./utils/scheduledFetch");
+const getCollectionsData = require("./utils/historyData");
+const listCollections = require("./db/collectionList");
+const getHistoryData = require("./utils/historyData");
+const {
+  getDailyBalance,
+  getWeeklyBalance,
+  getMonthlyBalance,
+} = require("./routes/balance.routes");
 
-//connect to database
-try {
-  dbConnect();
-} catch (error) {
-  throw new Error("Connection failed : ", { cause: error });
-}
+const main = async () => {
+  try {
+    await dbConnect();
+  } catch (error) {
+    throw new Error("Connection failed : ", error);
+  }
 
-app.use(express.json());
-app.use("/api", uploadWalletsandBalance); // to upload wallets and balance uncomment this
-app.use("/api", getWalletsAndBalance);
+  app.use(express.json());
+  app.use("/api", uploadWalletsandBalance); /// to upload wallets and balance uncomment this
+  app.use("/api", getWalletsAndBalance);
+  app.use("/api", getDailyBalance);
+  app.use("/api", getWeeklyBalance);
+  app.use("/api", getMonthlyBalance);
 
-scheduledTask();
+  await getHistoryData();
+  // await listCollections();
+  // console.log("in", await listCollections());
+  // console.log("starting scheduling task");
+  console.log("here index");
+  // scheduledTask();
 
-const port = 3000;
+  const port = 8000;
 
-app.listen(port, () => console.log("Listening to port " + port));
+  app.listen(port, () => console.log("Listening to port " + port));
+};
+(async () => {
+  try {
+    await main();
+  } catch (e) {
+    // Deal with the fact the chain failed
+  }
+})();
