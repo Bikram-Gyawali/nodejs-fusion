a167d2fe6a9a1935000f4bbd34d6397c0fab54b9
Bikram-Gyawali <bikramgyawali57@gmail.com>
2023-01-03 11:49:56 +0545

    changes in structure

--- remoteWork/db/dbConnect.js
+++ remoteWork/db/dbConnect.js
@@ -1,6 +1,7 @@
 const mongoose = require("mongoose");
 const dbConnect = async () => {
   await mongoose.connect(process.env.DB, { useNewUrlParser: true });
+  console.log("connected to database");
 };
 
 module.exports = dbConnect;
