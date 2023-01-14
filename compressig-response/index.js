ea8bb20fd4860f557cdf1a77aa0e9fc8a639c243
Bikram-Gyawali <bikramgyawali57@gmail.com>
2023-01-14 17:49:23 +0545

    compression

---
+++ compressig-response/index.js
@@ -0,0 +1,11 @@
+const express = require("express");
+const app = express();
+app.get("/", (req, res) => {
+  const favMarvelHero =
+    "SpiderMan , IronMan , Thor , Bikram , Dr.Strange and Wanda Maxim (Hot)";
+
+  res.send(favMarvelHero.repeat(1000));
+});
+app.listen(3000, function () {
+  console.log("Example app listening on port 3000!");
+});
