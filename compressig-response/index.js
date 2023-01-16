10868e290297e7da0239acd2279570e0aa14f26c
Bikram-Gyawali <bikramgyawali57@gmail.com>
2023-01-16 17:51:23 +0545

    compression system for response

--- compressig-response/index.js
+++ compressig-response/index.js
@@ -1,6 +1,28 @@
 const express = require("express");
 const app = express();
-app.use(compression());
+
+// we can do this default 
+// app.use(compression());
+
+// or do this
+const shouldCompress = (req, res) => {
+  if (req.headers['x-no-compression']) {
+    // Will not compress responses, if this header is present
+    return false;
+  }
+  // Resort to standard compression
+  return compression.filter(req, res);
+};// Compress all HTTP responses
+app.use(compression({
+  // filter: Decide if the answer should be compressed or not,
+  // depending on the 'shouldCompress' function above
+  filter: shouldCompress,
+  // threshold: It is the byte threshold for the response 
+  // body size before considering compression, the default is 1 kB
+  threshold: 0
+}));
+
+
 app.get("/", (req, res) => {
   const favMarvelHero =
     "SpiderMan , IronMan , Thor , Bikram , Dr.Strange and Wanda Maxim (Hot)";
