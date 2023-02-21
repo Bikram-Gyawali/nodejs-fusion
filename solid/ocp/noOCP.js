134023a41fb2d93715350efbfc47eb27a9a7bd4c
Bikram-Gyawali <bikramgyawali57@gmail.com>
2023-02-21 22:52:40 +0545

    open close

---
+++ solid/ocp/noOCP.js
@@ -0,0 +1,26 @@
+class Animal {
+  constructor(name, age, type) {
+    this.name = name
+    this.age = age
+    this.type = type
+  }
+
+  getSpeed() {
+    switch (this.type) {
+      case 'cheetah':
+        console.log('Cheetah runs up to 130mph ')
+        break
+      case 'lion':
+        console.log('Lion runs up to 80mph')
+        break
+      case 'elephant':
+        console.log('Elephant runs up to 40mph')
+        break
+      default:
+        throw new Error(`Unsupported animal type: ${this.type}`)
+    }
+  }
+}
+
+const animal1 = new Animal('Lion', 4, 'lion')
+animal1.getSpeed() // Lion runs up to 80mph
