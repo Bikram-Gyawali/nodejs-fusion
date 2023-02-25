935bc6ec114cb799ba2a50c52bf84362b0d0ba50
Bikram-Gyawali <bikramgyawali57@gmail.com>
2023-02-25 16:14:58 +0545

    fixed nolsp

---
+++ solid/lsp/LSP.js
@@ -0,0 +1,31 @@
+// The Liskov substitution principle is one of the most important principles to adhere to in object-oriented programming (OOP). It was introduced by the computer scientist Barbara Liskov in 1987 in a paper she co-authored with Jeannette Wing.
+
+// The principle states that child classes or subclasses must be substitutable for their parent classes or super classes. In other words, the child class must be able to replace the parent class. This has the advantage of letting you know what to expect from your code.
+
+
+class Animal {
+    constructor(name) {
+      this.name = name;
+    }
+  
+    makeSound() {
+      console.log(`${this.name} makes a sound`);
+    }
+  }
+  
+ 
+  
+  class Cat extends Animal {
+    makeSound() {
+      console.log(`${this.name} meows`);
+    }
+  }
+  
+  function makeAnimalSound(animal) {
+    animal.makeSound();
+  }
+  
+  const cheetah = new Animal('Cheetah');
+  makeAnimalSound(cheetah); // Cheetah makes a sound
+
+  
