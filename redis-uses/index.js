45b4b36baaddca02872e49fc2187c970bce22116
Bikram-Gyawali <bikramgyawali57@gmail.com>
2023-03-08 19:28:45 +0545

    basic redis client listening on channel

--- redis-uses/index.js
+++ redis-uses/index.js
@@ -8,3 +8,5 @@ client.on("message", (channel, message) => {
 });
 // Publish a message to the channel
 client.publish("channel1", "Hello, world!");
+
+// In this example, we first create a Redis client and then subscribe to the “channel1” channel. We then listen for messages on the channel using the on method and log any messages received to the console. Finally, we publish a message to the “channel1” channel using the publish method.
