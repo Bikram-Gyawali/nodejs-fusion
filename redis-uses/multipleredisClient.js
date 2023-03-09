2122b634c24813fb88bcd716ad8f58d9c0092a09
Bikram-Gyawali <bikramgyawali57@gmail.com>
2023-03-09 19:32:15 +0545

    multitple redis client listening on channel

--- redis-uses/multipleredisClient.js
+++ redis-uses/multipleredisClient.js
@@ -16,3 +16,7 @@ client2.on("message", function(channel, message) {
 for (let i = 0; i < 100000; i++) {
   client1.publish("channel1", "Hello, world!");
 }
+
+// In this example, we create two Redis clients and subscribe them both to the channel1 channel. We then publish 100,000 messages to the channel1 channel and observe that both clients receive the messages quickly and efficiently.
+
+// In conclusion, using Redis pub/sub for real-time communication in your applications can bring several advantages, such as scalability, flexibility, performance, and decoupling. By leveraging these benefits, you can build high-performing, scalable, and maintainable real-time applications.
