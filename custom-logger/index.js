8aafa6b421dfd380ef2a61b31bd1c2eee5601ae5
Bikram-Gyawali <bikramgyawali57@gmail.com>
2023-03-13 09:12:37 +0545

    logger basics

--- custom-logger/index.js
+++ custom-logger/index.js
@@ -15,3 +15,20 @@ const logger = winston.createLogger({
 })
 
 
+// the logger will be used in nodejs app as 
+app.app.get('/user', (req, res) => { 
+    // Fetch user data
+    logger.info('User data fetched successfully');
+    res.send(userData);
+  });
+  
+  app.post('/user', (req, res) => {
+    // Save user data to database
+    logger.info('User data saved successfully');
+    res.send({ message: 'User data saved successfully' });
+  });
+  
+  app.use((err, req, res, next) => {
+    logger.error('An error occurred', { error: err });
+    res.sendStatus(500);
+  });
