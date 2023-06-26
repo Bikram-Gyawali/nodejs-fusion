1d352eefc59b62e1d4b06f6d7d9880665660a10c
Bikram-Gyawali <bikramgyawali57@gmail.com>
2023-06-26 14:45:13 +0545

    database configs and app module update and main file updates for global prefix route

--- nest-crud-and-login/src/app.module.ts
+++ nest-crud-and-login/src/app.module.ts
@@ -2,9 +2,11 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { DatabaseModule } from './core/database/database.module';
+import { ConfigModule } from '@nestjs/config';
+import { UsersModule } from './modules/users/users.module';
 
 @Module({
-  imports: [DatabaseModule],
+  imports: [DatabaseModule, ConfigModule.forRoot({ isGlobal: true }), UsersModule],
   controllers: [AppController],
   providers: [AppService],
 })
--- nest-crud-and-login/src/core/database/database.module.ts
+++ nest-crud-and-login/src/core/database/database.module.ts
@@ -1,4 +1,9 @@
 import { Module } from '@nestjs/common';
+import { databaseProviders } from './database.providers';
 
-@Module({})
+@Module({
+  controllers: [],
+  providers: [...databaseProviders],
+  exports: [...databaseProviders],
+})
 export class DatabaseModule {}
