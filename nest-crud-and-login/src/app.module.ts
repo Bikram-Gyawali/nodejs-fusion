a5476711e60d87659306d4a99b9fda8f2c63acb6
Bikram-Gyawali <bikramgyawali57@gmail.com>
2023-06-27 15:35:21 +0545

    updated app module

--- nest-crud-and-login/src/app.module.ts
+++ nest-crud-and-login/src/app.module.ts
@@ -4,9 +4,10 @@ import { AppService } from './app.service';
 import { DatabaseModule } from './core/database/database.module';
 import { ConfigModule } from '@nestjs/config';
 import { UsersModule } from './modules/users/users.module';
+import { AuthModule } from './modules/auth/auth.module';
 
 @Module({
-  imports: [DatabaseModule, ConfigModule.forRoot({ isGlobal: true }), UsersModule],
+  imports: [DatabaseModule, ConfigModule.forRoot({ isGlobal: true }), UsersModule, AuthModule],
   controllers: [AppController],
   providers: [AppService],
 })
