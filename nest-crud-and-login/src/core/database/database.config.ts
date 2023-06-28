0daed5bee8cd3bf6641404d56ec289a713d8b628
Bikram-Gyawali <bikramgyawali57@gmail.com>
2023-06-28 14:08:34 +0545

    db config initialization and postgres setup

---
+++ nest-crud-and-login/src/core/database/database.config.ts
@@ -0,0 +1,34 @@
+import * as dotenv from 'dotenv';
+import { IDatabaseConfig } from './interfaces/dbConfig.interface';
+
+dotenv.config();
+
+export const databaseConfig: IDatabaseConfig = {
+  development: {
+    username: process.env.DB_USERNAME,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_DATABASE,
+    host: process.env.DB_HOST,
+    port: process.env.DB_PORT,
+    dialect: process.env.DB_DIALECT,
+    // urlDatabase: process.env.DB_URL_DATABASE,
+  },
+  test: {
+    username: process.env.DB_USERNAME,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_DATABASE,
+    host: process.env.DB_HOST,
+    port: process.env.DB_PORT,
+    dialect: process.env.DB_DIALECT,
+    // urlDatabase: process.env.DB_URL_DATABASE,
+  },
+  production: {
+    username: process.env.DB_USERNAME,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_DATABASE,
+    host: process.env.DB_HOST,
+    port: process.env.DB_PORT,
+    dialect: process.env.DB_DIALECT,
+    // urlDatabase: process.env.DB_URL_DATABASE,
+  },
+};
