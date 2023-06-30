206867fe705d9ceb2a8be4c5afe6d4707b832a57
Bikram-Gyawali <bikramgyawali57@gmail.com>
2023-06-30 22:26:24 +0545

    auth module created

--- nest-crud-and-login/src/modules/auth/auth.module.ts
+++ nest-crud-and-login/src/modules/auth/auth.module.ts
@@ -1,9 +1,12 @@
 import { Module } from '@nestjs/common';
 import { AuthService } from './auth.service';
 import { AuthController } from './auth.controller';
+import { PassportModule } from '@nestjs/passport';
+import { UsersModule } from '../users/users.module';
 
 @Module({
+  imports: [PassportModule, UsersModule],
   providers: [AuthService],
-  controllers: [AuthController]
+  controllers: [AuthController],
 })
 export class AuthModule {}
