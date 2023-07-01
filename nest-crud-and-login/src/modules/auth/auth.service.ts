768d198f4d5f72e9b2f654e7f0192d5895696f03
Bikram-Gyawali <bikramgyawali57@gmail.com>
2023-07-01 22:26:46 +0545

    auth service created

--- nest-crud-and-login/src/modules/auth/auth.service.ts
+++ nest-crud-and-login/src/modules/auth/auth.service.ts
@@ -1,4 +1,27 @@
 import { Injectable } from '@nestjs/common';
+import { UsersService } from '../users/users.service';
+import * as bcrypt from 'bcrypt';
 
 @Injectable()
-export class AuthService {}
+export class AuthService {
+  constructor(private readonly userService: UsersService) {}
+
+  async validateUser(username: string, pass: string) {
+    const user = await this.userService.findOneByEmail(username);
+    if (!user) {
+      return null;
+    }
+
+    const match = await this.comparePassword(pass, user.password);
+    if (!match) {
+      return null;
+    }
+
+    const { password, ...result } = user['dataValues'];
+    return result;
+  }
+
+  private async comparePassword(enteredPassword, dbPassword) {
+    const match = await bcrypt.compare(enteredPassword, dbPassword);
+  }
+}
