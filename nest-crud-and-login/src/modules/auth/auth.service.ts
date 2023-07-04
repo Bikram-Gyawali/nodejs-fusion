0377808306faa261a2d50a631094da3f895b030e
Bikram-Gyawali <bikramgyawali57@gmail.com>
2023-07-04 22:29:03 +0545

    fixed the issue of auth service of void return type

--- nest-crud-and-login/src/modules/auth/auth.service.ts
+++ nest-crud-and-login/src/modules/auth/auth.service.ts
@@ -7,21 +7,25 @@ export class AuthService {
   constructor(private readonly userService: UsersService) {}
 
   async validateUser(username: string, pass: string) {
+    // find if user exist with this email
     const user = await this.userService.findOneByEmail(username);
     if (!user) {
       return null;
     }
 
+    // find if user password match
     const match = await this.comparePassword(pass, user.password);
     if (!match) {
       return null;
     }
 
+    // tslint:disable-next-line: no-string-literal
     const { password, ...result } = user['dataValues'];
     return result;
   }
 
   private async comparePassword(enteredPassword, dbPassword) {
     const match = await bcrypt.compare(enteredPassword, dbPassword);
+    return match;
   }
 }
