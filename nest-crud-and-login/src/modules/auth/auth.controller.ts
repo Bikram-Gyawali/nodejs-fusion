3503d52bcd50ef48606b0c0d658df3b80eae38c8
Bikram-Gyawali <bikramgyawali57@gmail.com>
2023-07-24 23:09:08 +0545

    changes

--- nest-crud-and-login/src/modules/auth/auth.controller.ts
+++ nest-crud-and-login/src/modules/auth/auth.controller.ts
@@ -1,4 +1,22 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Post, UseGuards, Request, Body } from '@nestjs/common';
+import { AuthService } from './auth.service';
+import { AuthGuard } from '@nestjs/passport';
+import { UserDto } from '../users/dto/user.dto';
+import { DoesUserExist } from 'src/core/guards/doesUserExist.guard';
 
 @Controller('auth')
-export class AuthController {}
+export class AuthController {
+  constructor(private authService: AuthService) {}
+
+  @UseGuards(AuthGuard('local'))
+  @Post('login')
+  async login(@Request() req) {
+    return this.authService.login(req.user);
+  }
+
+  @UseGuards(DoesUserExist)
+  @Post('signup')
+  async signUp(@Body() user: UserDto) {
+    return await this.authService.create(user);
+  }
+}
