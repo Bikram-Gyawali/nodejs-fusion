0314b7d6ac626608b49577fa59e304bd497e6bad
Bikram-Gyawali <bikramgyawali57@gmail.com>
2023-07-25 23:09:23 +0545

    changes

--- nest-crud-and-login/src/modules/auth/auth.module.ts
+++ nest-crud-and-login/src/modules/auth/auth.module.ts
@@ -3,10 +3,22 @@ import { AuthService } from './auth.service';
 import { AuthController } from './auth.controller';
 import { PassportModule } from '@nestjs/passport';
 import { UsersModule } from '../users/users.module';
+import { JwtModule } from '@nestjs/jwt';
+import { LocalStrategy } from './local.strategy';
+import { JwtStrategy } from './jwt.strategy';
 
 @Module({
-  imports: [PassportModule, UsersModule],
-  providers: [AuthService],
+  imports: [
+    PassportModule,
+    UsersModule,
+    JwtModule.register({
+      secret: 'secret',
+      signOptions: {
+        expiresIn: 3600,
+      },
+    }),
+  ],
+  providers: [AuthService, LocalStrategy, JwtStrategy],
   controllers: [AuthController],
 })
 export class AuthModule {}
