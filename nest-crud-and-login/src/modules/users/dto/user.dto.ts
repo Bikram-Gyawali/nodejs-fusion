2b42f81f6defb1437c98d3f54008bcd834a66dd5
Bikram-Gyawali <bikramgyawali57@gmail.com>
2023-07-27 23:09:50 +0545

    changes

--- nest-crud-and-login/src/modules/users/dto/user.dto.ts
+++ nest-crud-and-login/src/modules/users/dto/user.dto.ts
@@ -1,17 +1,42 @@
+import { IsEnum, IsNotEmpty, MinLength, IsEmail } from 'class-validator';
+
 export class UserDto {
   readonly id: number;
+
+  @IsNotEmpty()
   readonly name: string;
+
+  @IsNotEmpty()
+  @IsEmail()
   readonly email: string;
+
+  @IsNotEmpty()
+  @MinLength(8)
   readonly password: string;
+
+  @IsNotEmpty()
   readonly role: string;
+
+  @IsNotEmpty()
   readonly avatar: string;
+
   readonly bio: string;
+
   readonly website: string;
+
+  @IsNotEmpty()
   readonly location: string;
+
+  @IsNotEmpty()
   readonly phone: string;
+
+  @IsNotEmpty()
+  @IsEnum(['male', 'female'])
   readonly gender: string;
+
+  @IsNotEmpty()
   readonly status: string;
+  readonly relationship: string;
   readonly createdAt: string;
   readonly updatedAt: string;
-  readonly relationship: string;
 }
